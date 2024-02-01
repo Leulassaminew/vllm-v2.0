e2475a0b68f0a29554f4e68ecf324add68a30c9b
Leulassaminew <88404832+Leulassaminew@users.noreply.github.com>
2024-02-01 18:43:19 +0300

    Update handler.py

--- src/handler.py
+++ src/handler.py
@@ -1,10 +1,43 @@
 import runpod
 from utils import JobInput
 from engine import vLLMEngine
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from datasets import load_dataset
+import pandas as pd
 
+dataset=load_dataset("meetplace1/clientdata.csv")
+df = pd.DataFrame(dataset['train'])
+sentences = df["Question"].values
+vectorizer = TfidfVectorizer()
+vectorizer.fit(sentences)
+sentence_vectors = vectorizer.transform(sentences)
 vllm_engine = vLLMEngine()
 
+def context(job_input):
+    messages=job_input["messages"][-1]
+    messages=messages["content"]
+    text_vector = vectorizer.transform([messages])
+    similarity_scores = cosine_similarity(text_vector, sentence_vectors)
+    most_similar_index = similarity_scores.argmax()
+    top_indices = similarity_scores.argsort()[0][-2:]
+    top=top_indices[::-1]
+    top_sentences = sentences[top]
+    answer = ''
+    if similarity_scores[0][most_similar_index] >= 0.4:
+        for sent in top_sentences:
+            res = df[df["Question"] == sent]["Answer"].values[0]
+            answer += res
+            # txt+=res
+            answer = answer.replace("•", " ")
+            answer = answer.replace("\n", "")
+    else:
+        answer = ''
+    return answer
+
 async def handler(job):
+    cont=context(job["input"])
+    job["input"]["context"]=cont
     job_input = JobInput(job["input"])
     results_generator = vllm_engine.generate(job_input)
     async for batch in results_generator:
